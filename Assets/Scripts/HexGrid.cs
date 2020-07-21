7e2330f8ccc706a65422dca391e948cd7c21c845
Nicolas Lépy <33573107+nicolasLepy@users.noreply.github.com>
2020-07-21 11:54:36 +0200

    Initial commit

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+
+    public int width => 6;
+    public int height => 6;
+
+    [SerializeField]
+    private HexCell cellPrefab;
+
+    private HexCell[] cells;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
